7d95f4e12a6c47d26afb835dfeb8ac348de93eb5
aidan <aidankhogg@gmail.com>
2025-01-30 10:52:19 +0000

    start of base framework

---
+++ pythings/base.py
@@ -0,0 +1,64 @@
+from abc import ABC, abstractmethod
+
+
+class BaseThing(ABC):
+    """
+    Base class for all **Things**.
+
+    A *Thing* is a generic abstraction for anything, composable by default.
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        Initialise an instance of a **Thing**.
+        """
+        pass
+
+class Thing(BaseThing):
+    """
+    A generic abstraction for anything, composable by default.
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        Initialise an instance of a **Thing**.
+        """
+        super().__init__()
+
+class Attribute(BaseThing):
+    """
+    A generic abstraction for an *attribute* of a **Thing**.
+    """
+
+    @abstractmethod
+    def __init__(self):
+        super().__init__()
+
+class Metadata(BaseThing):
+    """
+    A generic abstraction for *metadata* of a **Thing**.
+    """
+
+    @abstractmethod
+    def __init__(self):
+        super().__init__()
+
+class BaseRelationship(BaseThing):
+    """
+    Base class for all **relationship** classes.
+    """
+
+    @abstractmethod
+    def __init__(self):
+        super().__init__()
+
+class Relationship(BaseRelationship):
+    """
+    Represents a *relationship* between **Things**.
+    """
+
+    @abstractmethod
+    def __init__(self):
+        super().__init__()
