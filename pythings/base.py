afd871505088ecf86ead6a39e65a4f76552311a4
aidan <aidankhogg@gmail.com>
2025-01-31 11:49:29 +0000

    added tests
    - base classes
    - things

--- pythings/base.py
+++ pythings/base.py
@@ -1,5 +1,6 @@
+import json
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Dict
 from uuid import UUID, uuid4
 
 """
@@ -20,6 +21,21 @@ class BaseThing(ABC):
         """
         pass
 
+    @abstractmethod
+    def to_dict(self) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def from_dict(cls, data: Dict[str, Any]) -> 'BaseThing':
+        pass
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
+
+    @classmethod
+    def from_json(cls, data: str) -> 'BaseThing':
+        return cls.from_dict(json.loads(data))
+
 
 class Thing(BaseThing):
     """
@@ -32,6 +48,25 @@ class Thing(BaseThing):
         """
         super().__init__()
         self._metadict = Metadata()
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'metadict': self._metadict.to_dict()
+        }
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> 'Thing':
+        instance = cls()
+        instance._metadict = Metadata.from_dict(data['metadict'])
+        return instance
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Thing):
+            return False
+        return self._metadict == other._metadict
+
+    def __hash__(self) -> int:
+        return hash(self._metadict)
 
 
 class FoundationalThing(Thing):
@@ -50,6 +85,32 @@ class Attribute(BaseThing):
 
     def __init__(self, name: str, value: Any, description: str = None):
         super().__init__()
+        self.name = name
+        self.value = value
+        self.description = description
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'name': self.name,
+            'value': self.value,
+            'description': self.description
+        }
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> 'Attribute':
+        return cls(
+            name=data['name'],
+            value=data['value'],
+            description=data.get('description')
+        )
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Attribute):
+            return False
+        return (self.name, self.value, self.description) == (other.name, other.value, other.description)
+
+    def __hash__(self) -> int:
+        return hash((self.name, self.value, self.description))
 
 class Metadata(BaseThing):
     """
@@ -59,11 +120,12 @@ class Metadata(BaseThing):
     def __init__(self, uid: UUID | str | None = None):
         """
         Initialise an instance of **Metadata**.
+
         :param uid:
         """
         super().__init__()
         self._metadict = {
-            "uuid": uid if uid is not None else uuid4()
+            "uid": uid if uid is not None else uuid4()
 
         }
         self._mappings = {
@@ -77,7 +139,7 @@ class Metadata(BaseThing):
     def metadata(self):
         """
         Get the metadata.
-        :return: (dict) metadata
+        :return: (*dict*) metadata
         """
         return self._metadict
 
@@ -85,7 +147,7 @@ class Metadata(BaseThing):
         """
         Get an attribute of the metadata.
         :param item: metadata key.
-        :return:
+        :return: Value of the metadata key.
         """
         keys = item.split('.')
         value = self._metadict
@@ -93,19 +155,11 @@ class Metadata(BaseThing):
             value = value[key]
         return value
 
-    def __setattr__(self, key, value):
+    def __repr__(self):
         """
-        Set an attribute of the metadata.
-        :param key: metadata key.
-        :param value: metadata value.
-        :return:
+        :return: (str) representation of the metadata.
         """
-        try:
-            self._metadict[key] = value
-        except KeyError as e:
-            raise e
-        finally:  # TODO Audit/Event logic here
-            pass
+        return f"Metadata({self._metadict})"
 
     def get_mappings(self):
         """
@@ -121,6 +175,26 @@ class Metadata(BaseThing):
         """
         return self.get_mappings()
 
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'uid': str(self._metadict['uid']),
+            'mappings': self._mappings
+        }
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> 'Metadata':
+        instance = cls(uid=data['uid'])
+        instance._mappings = data['mappings']
+        return instance
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Metadata):
+            return False
+        return self._metadict == other._metadict
+
+    def __hash__(self) -> int:
+        return hash(self._metadict['uid'])
+
 
 class BaseRelationship(BaseThing):
     """
@@ -130,10 +204,29 @@ class BaseRelationship(BaseThing):
     @abstractmethod
     def __init__(self):
         """
-
+        Initialize an instance of a **BaseRelationship**.
         """
         super().__init__()
         self.metadata = Metadata()
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            'metadata': self.metadata.to_dict()
+        }
+
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> 'BaseRelationship':
+        instance = cls()
+        instance.metadata = Metadata.from_dict(data['metadata'])
+        return instance
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, BaseRelationship):
+            return False
+        return self.metadata == other.metadata
+
+    def __hash__(self) -> int:
+        return hash(self.metadata)
 
 
 class Relationship(BaseRelationship):
@@ -143,7 +236,7 @@ class Relationship(BaseRelationship):
 
     def __init__(self):
         """
-
+        Initialize an instance of a **Relationship**.
         """
         super().__init__()
 
@@ -156,7 +249,7 @@ class BaseInterface(ABC):
     @abstractmethod
     def __init__(self):
         """
-
+        Initialize an instance of a **BaseInterface**.
         """
         pass
 
@@ -167,7 +260,7 @@ class Interface(BaseInterface):
 
     def __init__(self):
         """
-
+        Initialize an instance of an **Interface**.
         """
         super().__init__()
 
