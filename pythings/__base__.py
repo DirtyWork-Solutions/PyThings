0714c28f18b1bda36ea058921188e9d4e80722df
aidan <aidankhogg@gmail.com>
2025-02-03 18:54:22 +0000

    Project Core
    - structural files

---
+++ pythings/__base__.py
@@ -0,0 +1,112 @@
+"""
+TODO: root module docs
+"""
+
+from abc import abstractmethod, ABC
+
+from pythings.__errors__ import PyThingsException
+from utils.patterns.mixins import MetadataMixin, MappingsMixin
+
+class BaseEntity(ABC):  # TODO: Implement base class
+    """
+    TODO: Class docs
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        TODO: Method docs
+        """
+
+        pass
+
+    def __repr__(self):
+        return f"Entity()"
+
+class BasePhysicalEntity(BaseEntity, ABC):  # TODO: Implement base class
+    """
+    TODO: Class docs
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        TODO: Method docs
+        """
+        super().__init__()
+
+    def __repr__(self):
+        return f"PhysicalEntity()"
+
+
+class BaseAbstractEntity(BaseEntity, ABC):  # TODO: Implement base class
+    """
+    TODO: Class docs
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        TODO: method docs
+        """
+        super().__init__()
+
+    def __repr__(self):
+        return f"PhysicalEntity()"
+
+
+class BaseAttribute(BaseAbstractEntity,ABC):  # TODO: Implement base class
+    """
+    TODO: Class docs
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        TODO: Method docs
+        """
+        super().__init__()
+
+
+class BaseMeta(ABC):  # TODO: Implement base class
+    """
+    TODO: Class docs
+    """
+
+    @abstractmethod
+    def __init__(self):
+        """
+        TODO: Method docs
+        """
+
+    def get_all_meta(self, as_dict: bool = True):  # TODO: Implement method
+        """
+        TODO: method docs
+
+        :param as_dict:
+        :return:
+        """
+        pass
+
+    def get_meta(self, category_name: str):  # TODO: Implement method
+        """
+        Get a whole meta category/group
+
+        :param category_name:
+        :return:
+        """
+        pass
+
+    def get_metakey(self, key_name: str, category: str = None, soft_fail: bool = False):  # TODO: Implement method
+        """
+        Get a single meta-value from the keyname. *Optionally specific the category/group, otherwise return first result
+        if any.*
+
+        :param key_name: (str) | name of the dict key.
+        :param category: (str) | category/group to filter in search, if any.
+        :param soft_fail: (bool) | return None in case of error/lack of result? *Default is raise error*.
+        :return: (Dict)
+        :raises KeyError: (Key Not Found) | if key doesn't exist.
+        :raises ValueError: (Category Not Found) | if the category/group dict is not found.
+        """
+        pass
