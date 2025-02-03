34517bd5d09cbeae0a074dc02b12adaafa4178d9
aidan <aidankhogg@gmail.com>
2025-02-03 19:12:57 +0000

    Project Core
    - library exceptions/errors

---
+++ pythings/__errors__.py
@@ -0,0 +1,10 @@
+"""
+The errors and exceptions-related code for the PyThings package.
+"""
+
+class PyThingsException(Exception):
+    """
+    Base class for all **PyThings** exceptions.
+    """
+    pass
+
