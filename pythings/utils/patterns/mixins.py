c11c0e92420288251e720cb5350988cff86aff2f
aidan <aidankhogg@gmail.com>
2025-02-07 02:53:37 +0000

    purged futures

--- pythings/utils/patterns/mixins.py
+++ pythings/utils/patterns/mixins.py
@@ -4,7 +4,6 @@ TODO: module docs
 
 from abc import ABC, abstractmethod
 from typing import List, AnyStr
-from pythings.__base__ import BaseMeta
 from pythings.__errors__ import PyThingsException
 
 
@@ -19,57 +18,3 @@ class BaseMixin(ABC):  # TODO: Make a part of PyExpand
         TODO: method docs
         """
         pass
-
-
-class Metadata(BaseMeta):  # TODO: Move?
-    """
-    TODO: class docs
-    """
-
-    def __init__(self):
-        """
-
-        """
-        super().__init__()
-
-
-class MetadataMixin(BaseMixin):  # TODO: Implement class
-    """
-    TODO: Class docs
-    """
-
-    # Default meta-dict schemas
-    _schemas = {
-        "def_meta": {
-        },
-        "def_mappings": {
-
-        }
-    }
-
-    def __init__(self, apply_schemas: str | List[AnyStr] = None):
-        """
-        TODO: method docs
-
-        :param apply_schemas:
-        """
-        super().__init__()
-        self._meta = Metadata()
-
-        #
-        if apply_schemas:
-            pass
-        else:
-            pass
-
-
-class MappingsMixin(BaseMixin, MetadataMixin):  # TODO: Implement class
-    """
-    TODO: class docs
-    """
-
-    def __init__(self):
-        """
-        TODO: method docs
-        """
-        super().__init__()
--- pythings/utils/persistence/formats.py
+++
@@ -1,8 +0,0 @@
-# TODO: OWL Format
-# TODO: JSON-LD Format
-# TODO: RDF & SPARQL
-# TODO: Knowledge Interchange Format (KIF)
-# TODO: Agent Communication Language (ACL)
-# TODO: Knowledge Query and Manipulation Language (KQML)
-# TODO: Common Logic
-# T
